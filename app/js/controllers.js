fb3b74354f7c008f699a8bc8719739362231a545
Dmitry N <ndk@esperanca.biz>
2013-10-28 23:38:54 -0500

    modified:   app/index.html 	modified:   app/js/app.js 	modified:   app/js/controllers.js 	modified:   app/js/directives.js 	modified:   app/js/filters.js 	modified:   app/less/style.less 	modified:   test/lib/angular/angular-mocks.js 	modified:   test/lib/angular/angular-scenario.js 	modified:   test/lib/angular/version.txt 	app/db.php 	app/js/ui/ 	app/login.php

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,21 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.Submit = function () {
+	var credentials = {
+	    'username' : $scope.username,
+	    'password' : CryptoJS.SHA256($scope.password).toString(CryptoJS.enc.Hex)
+	};
+	$http.post('login.php', credentials)
+	    .success(function(data, status, header, config) {
+		$scope.error = "";
+		console.log(data);
+	    })
+	    .error(function(data, status, header, config) {
+		$scope.error = data.error;
+	    });
+    }
   }])
   .controller('MyCtrl2', [function() {
 
