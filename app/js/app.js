c0b69d56c21c74cf5384481f42a05875d2d18e4c
Dmitry N <ndk@esperanca.biz>
2013-10-29 22:59:27 -0500

    cleaning up

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,17 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', 
-    { templateUrl: 'section/login.html',
-      controller: 'LoginCtrl' } );
-  $routeProvider.when('/manager',
-    { templateUrl: 'section/manager.html', controller: 'ManagerCtrl' } );
-      $routeProvider.otherwise( { redirectTo: '/login' } );
+angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/login', {
+			templateUrl : 'section/login.html',
+			controller : 'LoginCtrl'
+		});
+		$routeProvider.when('/manager', {
+			templateUrl : 'section/manager.html',
+			controller : 'ManagerCtrl'
+		});
+		$routeProvider.otherwise({
+			redirectTo : '/login'
+		});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,41 +2,40 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('LoginCtrl', ['$scope', '$http', '$location', '$route', '$window', function($scope, $http, $location, $route, $window) {
-    $window.document.title = 'Login';
-    $scope.Submit = function () {
-	var credentials = {
-	    'username' : $scope.username,
-	    'password' : CryptoJS.SHA256($scope.password).toString(CryptoJS.enc.Hex)
-	};
-	$http.post('login.php', credentials)
-	    .success(function(data, status, header, config) {
-		$scope.error = "";
-		console.log(data);
-		$scope.access = data.access;
-		switch($scope.access) {
-			case '0':
-				$location.path('/manager');
-				break;
-			case '1':
-				$location.path('/kitchen');
-				break;
-			case '2':
-				$location.path('/waiter');
-				break;
-			case '3':
-				$location.path('/table');
-				break;
-			default:
-				$location.path('/login');
-		}
-	    })
-	    .error(function(data, status, header, config) {
-		$scope.error = data.error;
-	    });
-    }
-  }])
-  .controller('ManagerCtrl', ['$scope', '$http', '$location', '$route', '$window', function($scope, $http, $location, $route, $window) {
+angular.module('myApp.controllers', [])
+	.controller('LoginCtrl', ['$scope', '$http', '$location', '$route', '$window', 
+		function($scope, $http, $location, $route, $window) {
+			$window.document.title = 'Login';
+			$scope.Submit = function() {
+				var credentials = {
+					username' : $scope.username,
+					'password' : CryptoJS.SHA256($scope.password).toString(CryptoJS.enc.Hex)
+				};
+				$http.post('login.php', credentials)
+					.success(function(data, status, header, config) {
+						$scope.error = "";
+						console.log(data);
+						$scope.access = data.access;
+						switch($scope.access) {
+							case '0':
+								$location.path('/manager');
+								break;
+							case '1':
+								$location.path('/kitchen');
+								break;
+							case '2':
+								$location.path('/waiter');
+								break;
+							case '3':
+								$location.path('/table');
+								break;
+							default:
+								$location.path('/login');
+						}})
+					.error(function(data, status, header, config) {
+						$scope.error = data.error;
+		})}}])
+	.controller('ManagerCtrl', ['$scope', '$http', '$location', '$route', '$window',
+		function($scope, $http, $location, $route, $window) {
 
-  }]);
+}]);
