bfea7f855ea8b5b5175b6a560dae21378228785d
Dmitry N <ndk@esperanca.biz>
2013-10-31 00:18:44 -0500

    modified:   app/index.html 	modified:   app/js/controllers.js 	modified:   app/js/services.js 	modified:   app/less/style.less 	modified:   app/section/manager.html 	deleted:    app/section/manager/menuitem.html 	app/section/manager/

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,8 @@
 
 angular.module('myApp.controllers', [])
     .controller('LoginCtrl', ['$scope', '$http', '$location', '$route',
-		    '$window', function($scope, $http, $location,
-			    $route, $window) {
+		    '$window', '$rootScope', function($scope, $http, $location,
+			    $route, $window, $rootScope) {
 	    $window.document.title = 'Login';
 	    $scope.Submit = function() {
 		var credentials = {
@@ -13,51 +13,59 @@ angular.module('myApp.controllers', [])
 		    'password' : CryptoJS.SHA256($scope.password)
 				    .toString(CryptoJS.enc.Hex)
 		};
-		$http.post('/login.php', credentials)
+		$http.post('login.php', credentials, $rootScope)
 		    .success(function(data, status, header, config) {
 			$scope.error = "";
-			$scope.access = data.access;
-			switch($scope.access) {
-			    case '0':
+			$rootScope.access = parseInt(data.access);
+			switch($rootScope.access) {
+			    case 0:
 				$location.path('/manager');
 				break;
-			    case '1':
+			    case 1:
 				$location.path('/kitchen');
 				break;
-			    case '2':
+			    case 2:
 				$location.path('/waiter');
 				break;
-			    case '3':
+			    case 3:
 				$location.path('/table');
 				break;
 			    default:
 				$location.path('/login');
 		    }})
 		    .error(function(data, status, header, config) {
-			$scope.error = data.error;
+			console.log(data);
     })}}])
-    .controller('ManagerCtrl', ['$scope', '$http', '$location', '$route',
-		    '$window', function($scope, $http, $location, $route, 
-			    $window) {
+    .controller('ManagerCtrl', ['$rootScope', '$scope', '$location', '$route',
+		'$window', function ($rootScope, $scope, $location, $route,
+		$window) {
+//	if ($rootScope.access == null)
+//	    $location.path('/login');
+	$rootScope.section = 'menu';
 	$window.document.title = 'Manager Section';
-	$http.get('db.php/menucategory')
+    }])
+    .controller('mMenuCtrl', ['$scope', '$location', '$route', '$window',
+		'MenuCategory', 'MenuItem', function ($scope, $location,
+		$route, $window, MenuCategory, MenuItem) {
+	$scope.mc = new Object();
+	$scope.menucategory = MenuCategory.query(function () {
+	    $scope.mc.id = $scope.menucategory[0].id;
+	});
+	$scope.mc.idx = 0;
+	$scope.menuitem = MenuItem.query();
+//	console.log(mc_idx);
+/*	while ($scope.menucategory == null) {
+	    console.log($scope.menucategory);
+	    console.log($scope.mc_id);
+	} 
+	if ($routeParams.idx != null)
+	    $scope.mc_idx = parseInt($routeParams.idx);
+	$http.get('db.php/menuitem/mc_id/'+$scope.menucategory[$scope.mc_idx].id)
 	    .success(function(data, status, header, config) {
-		$scope.menucategory = data;
-		$scope.menuitem = new Object;
-		console.log($scope.menucategory);
-		for (var key in $scope.menucategory) {
-		    $http.get('db.php/menuitem/mc_id/'+$scope.menucategory[key].id)
-			.success(function(data, status, header, config) {
-			    $scope.menuitem[key] = data;
-			})
-			.error(function(data, status, header, config) {
-			    console.log(data);
-			    $scope.error = data;
-			})
-		}
+		$scope.menuitem = data;
 	    })
 	    .error(function(data, status, header, config) {
-		console.log(data);
-		$scope.error = data;
-	    });
+	        console.log(data);
+	        $scope.error = data;
+	    })*/
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -3,7 +3,32 @@
 /* Services */
 
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource'])
+    .factory('MenuCategory', [ '$resource', function ($resource) {
+	return $resource('db.php/menucategory/:id', {}, {
+	    query: {
+		method: 'GET',
+		params: { id: '' },
+		isArray: true
+	    }
+	})
+    }])
+    .factory('MenuItem', [ '$resource', function ($resource) {
+	return $resource('db.php/menuitem/:cmd:id', {}, {
+	    get: {
+		method: 'GET',
+		params: {
+		    cmd: '',
+		    id: '@id'
+		}
+	    },
+	    query: {
+		method: 'GET',
+		params: {
+		    cmd: '@cmd',
+		    id: '@id' },
+		isArray: true
+	    }
+	})
+    }]);
+
